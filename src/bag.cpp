c49abd045209b2d174a430ae603b06cd67314df1
Jacques Heunis <jaaksemail@gmail.com>
2016-04-09 17:41:20 +0200

    Rework the bag motion to be more responsive and easy to work with

--- src/bag.cpp
+++ src/bag.cpp
@@ -10,24 +10,41 @@ Bag::Bag(dge::Vector2 _position, dge::Vector2 _size, int _category, Level* _leve
 
 void Bag::updatePosition(float timestep)
 {
-    position = dge::slide(position, nextPosition, timestep);
+    dge::Vector2I currentLoc(dge::round(position.x), dge::round(position.y));
+    MapObject* currentLocObj = level->map[currentLoc.y][currentLoc.x];
+    nextPosition = currentLoc;
+    if ((currentLocObj != nullptr) && (currentLocObj->type == MapObjectType::conveyer))
+    {
+        dge::Vector2I conveyerMotion = ((Conveyer*)currentLocObj)->getSpeed();
+        dge::Vector2I newNextPosition = currentLoc+conveyerMotion;
+        MapObject* nextPositionObj = level->map[newNextPosition.x][newNextPosition.y];
+        if((nextPositionObj == nullptr) || (nextPositionObj->type !=MapObjectType::wall))
+        {
+            nextPosition = newNextPosition;
+        }
+    }
 
-    float xOffset = position.x - nextPosition.x;
-    float yOffset = position.y - nextPosition.y;
+    // NOTE: We move in each direction independently so that we don't end up moving very slowly
+    //       in the perpendicular direction after turning a corner onto a long straight.
+    //       This means that we need to manually constrain the movement in each direction in order
+    //       to maintain a constant movement speed
+    float maxMoveDistance = timestep;
+    float xOffset;
+    float yOffset;
+    xOffset = nextPosition.x - position.x;
+    yOffset = nextPosition.y - position.y;
+    if(abs(xOffset*yOffset) > 0.01f)
+    {
+        maxMoveDistance *= 1.0f/ROOTTWO;
+    }
+    position.x = dge::slide(position.x, nextPosition.x, maxMoveDistance);
+    position.y = dge::slide(position.y, nextPosition.y, maxMoveDistance);
+
+    xOffset = position.x - nextPosition.x;
+    yOffset = position.y - nextPosition.y;
     float offsetDist = xOffset*xOffset + yOffset*yOffset;
     if(offsetDist < 0.1f*0.1f)
     {
         lastPosition = nextPosition;
-        MapObject* currentPositionObj = level->map[nextPosition.y][nextPosition.x];
-        if ((currentPositionObj != nullptr) && (currentPositionObj->type == MapObjectType::conveyer))
-        {
-            nextPosition += ((Conveyer*)level->map[lastPosition.y][lastPosition.x])->getSpeed();
-        }
-        MapObject* nextPositionObj = level->map[nextPosition.y][nextPosition.x];
-        if ((nextPositionObj != nullptr) && (nextPositionObj->type == MapObjectType::wall))
-        {
-            nextPosition = lastPosition;
-        }
     }
-
 }
